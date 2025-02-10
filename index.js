4454408e260c8a53de6a3ebbaec9b170041602af
arturrr03 <rawungito@gmail.com>
2025-02-10 09:13:08 +0800

    express

--- index.js
+++ index.js
@@ -1,45 +1,13 @@
-const { log } = require('console');
 const http = require('http');
-const {data} = require('./users')
-const moment = require("moment")
-const server = http.createServer ((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/json');
-    // res.write(hello)
-    // res.write(greetings())
-    const url = req.url;
-    if(url ==='/') {
-        res.write('This is the home page')
-    }
-    else if (url === '/about'){
-        res.setHeader('Content-Type', 'text/json');
-        res.write(JSON.stringify({
-            status: 'success',
-            message: 'response success',
-            date: moment().format()
-        }))
-    }
-    else if (url === '/users'){
-        res.write(data())
-    }
-    else{
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/json');
-        res.write(JSON.stringify({
-            status: 'not found',
-            message: 'Route tidak ada',
-            date: moment().format('MMMM Do YYYY, h:mm:ss a')
-        }))
-        
+const express = require('express')
+const moment = require('moment')
+const app = express()
 
-    }
-
-    res.end();
-})
+app.get('/', (req, res) => res.send('Hello World'))
 
 const hostname = '127.0.0.1';
 const port = 3000;
-server.listen(port,hostname, ()=>{
+app.listen(port,hostname, ()=>{
     console.log(`Server running at http://${hostname}:${port}/`);
     
 });
