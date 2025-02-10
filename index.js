15eed7832765a99ec32317ff82bd52d19b8a9ecc
arturrr03 <rawungito@gmail.com>
2025-02-10 09:33:52 +0800

    request

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const moment = require('moment')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World'))
+app.get('/about', (req, res) => res.status(200).json({
+    status : 'success',
+    message: 'About page',
+    data: []
+}))
+app.post('/contoh', (req, res) => res.send('Request method POST'))
+app.put('/contoh', (req, res) => res.send('Request method PUT'))
+app.delete('/contoh', (req, res) => res.send('Request method DELETE'))
+app.patch('/contoh', (req, res) => res.send('Request method PATCH'))
+
 
 const hostname = '127.0.0.1';
 const port = 3000;
